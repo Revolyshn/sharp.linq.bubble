c1466d1b011b46cb529a57ad82c11aab92ad66c6
student <student@K21-09-STUDENT>
2021-05-15 09:19:57 +0500

    project 2

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void CWL(string txt) => Console.WriteLine(txt);
+		static void CW(string txt) => Console.Write(txt);
+		static Random rnd = new Random(Environment.TickCount);
+
+		public static List<int> GetList(int size)
+		{
+			var tmp = new List<int>();
+
+			for (int i = 0; i < size; i++) tmp.Add(rnd.Next(0, 10));
+
+			return tmp;
+		}
+
+
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				try
+				{
+					CW("Введите размер массива: ");
+
+					int size = int.Parse(Console.ReadLine());
+					List<int> buffer = GetList(size);
+					//buffer.Sort();
+					buffer.GroupBy(x => buffer);
+					CWL("press f");
+				}
+				catch (Exception er)
+				{
+					CWL(er.ToString());
+				}
+
+				Console.ReadLine();
+			}
+			
+		}
+	}
+}
